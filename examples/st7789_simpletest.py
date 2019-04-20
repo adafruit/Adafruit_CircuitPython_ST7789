da41abc8008237c597f8e56b06564a851d7a5c60
Melissa LeBlanc-Williams <melissa@Melissas-MacBook-Pro.local>
2019-04-20 16:47:52 -0700

    Change SPI to 24MHz

--- examples/st7789_simpletest.py
+++ examples/st7789_simpletest.py
@@ -8,6 +8,10 @@ import displayio
 from adafruit_st7789 import ST7789
 
 spi = board.SPI()
+while not spi.try_lock():
+  pass
+spi.configure(baudrate=24000000) # Configure SPI for 24MHz
+spi.unlock()
 tft_cs = board.D5
 tft_dc = board.D6
 
