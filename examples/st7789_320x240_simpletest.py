ea31ab15cb45d50cca6ad1beb72cc519a819560d
Melissa LeBlanc-Williams <melissa@adafruit.com>
2019-08-08 11:31:24 -0700

    Updated examples to be more comprehensive

--- examples/st7789_320x240_simpletest.py
+++ examples/st7789_320x240_simpletest.py
@@ -1,10 +1,11 @@
 """
-This test will initialize the display using displayio
-and draw a solid red background
+This test will initialize the display using displayio and draw a solid green
+background, a smaller purple rectangle, and some yellow text.
 """
-
 import board
 import displayio
+import terminalio
+from adafruit_display_text import label
 from adafruit_st7789 import ST7789
 
 spi = board.SPI()
@@ -26,12 +27,28 @@ display.show(splash)
 
 color_bitmap = displayio.Bitmap(320, 240, 1)
 color_palette = displayio.Palette(1)
-color_palette[0] = 0xFF0000
+color_palette[0] = 0x00FF00 # Bright Green
 
 bg_sprite = displayio.TileGrid(color_bitmap,
                                pixel_shader=color_palette,
                                x=0, y=0)
 splash.append(bg_sprite)
+
+# Draw a smaller inner rectangle
+inner_bitmap = displayio.Bitmap(280, 200, 1)
+inner_palette = displayio.Palette(1)
+inner_palette[0] = 0xAA0088 # Purple
+inner_sprite = displayio.TileGrid(inner_bitmap,
+                                  pixel_shader=inner_palette,
+                                  x=20, y=20)
+splash.append(inner_sprite)
+
+# Draw a label
+text_group = displayio.Group(max_size=10, scale=3, x=57, y=120)
+text = "Hello World!"
+text_area = label.Label(terminalio.FONT, text=text, color=0xFFFF00)
+text_group.append(text_area) # Subgroup for text scaling
+splash.append(text_group)
 
 while True:
     pass
