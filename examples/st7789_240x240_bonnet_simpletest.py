cba0a9bb6abf5960ed8f316bacea42cd986dad3e
dherrada <dylan.herrada@adafruit.com>
2021-01-13 11:51:29 -0500

    Ran pre-commit, added licenses

--- examples/st7789_240x240_bonnet_simpletest.py
+++ examples/st7789_240x240_bonnet_simpletest.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
 """
 This test will initialize the display using displayio and draw a solid green
 background, a smaller purple rectangle, and some yellow text.
