64e02070e1053db6655a77be299d0304a21776e2
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:14 -0400

    Update version string

--- adafruit_st7789.py
+++ adafruit_st7789.py
@@ -42,7 +42,7 @@ Implementation Notes
 
 import displayio
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_ST7789.git"
 
 _INIT_SEQUENCE = (
