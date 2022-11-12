54f169eada4099cead953f7a234ad18ccbb66ffc
Mark McGookin <mark.mcgookin@gmail.com>
2022-11-12 23:02:06 +0000

    Small changes to work with Circuit Python 8 Beta 4 on Raspberry Pi Pico. board.SPI didn't exist and needed to be changed to busio.SPI. The Dx reference for pins doesn't work and needs updated to be GPx (for the raspberry pi pico at least)

--- examples/st7789_172x320_1.47_simpletest.py
+++ examples/st7789_172x320_1.47_simpletest.py
@@ -3,25 +3,26 @@
 
 """
 This test will initialize the display using displayio and draw a solid green
-background, a smaller purple rectangle, and some yellow text.
+background, a smaller purple rectangle, and some yellow text..
 """
 import board
+import busio
 import terminalio
 import displayio
 from adafruit_display_text import label
 from adafruit_st7789 import ST7789
 
-BORDER_WIDTH = 20
+BORDER_WIDTH = 28
 TEXT_SCALE = 3
 
 # Release any resources currently in use for the displays
 displayio.release_displays()
 
-spi = board.SPI()
-tft_cs = board.D5
-tft_dc = board.D6
-tft_rst = board.D9
+tft_cs = board.GP5 #board.D5
+tft_dc = board.GP6 #board.D6
+tft_rst = board.GP7 #board.D7
 
+spi = busio.SPI(board.GP2, board.GP3, board.GP4)
 display_bus = displayio.FourWire(spi, command=tft_dc, chip_select=tft_cs, reset=tft_rst)
 
 display = ST7789(display_bus, width=320, height=172, colstart=34, rotation=270)
@@ -34,12 +35,13 @@ color_bitmap = displayio.Bitmap(display.width, display.height, 1)
 color_palette = displayio.Palette(1)
 color_palette[0] = 0x00FF00  # Bright Green
 bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
-splash.append(bg_sprite)
+splash.append(bg_sprite) 
 
 # Draw a smaller inner rectangle
 inner_bitmap = displayio.Bitmap(
     display.width - (BORDER_WIDTH * 2), display.height - (BORDER_WIDTH * 2), 1
 )
+
 inner_palette = displayio.Palette(1)
 inner_palette[0] = 0xAA0088  # Purple
 inner_sprite = displayio.TileGrid(
@@ -50,7 +52,7 @@ splash.append(inner_sprite)
 # Draw a label
 text_area = label.Label(
     terminalio.FONT,
-    text="Hello World!",
+    text="Hello World!!",
     color=0xFFFF00,
     scale=TEXT_SCALE,
     anchor_point=(0.5, 0.5),
